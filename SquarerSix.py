7ba065d920a4d399569f828d9f975cf1fe747c54
SquarerFive <aidensoedjarwo@gmail.com>
2017-11-25 17:02:57 +1100

    Add files via upload

---
+++ SquarerSix.py
@@ -0,0 +1,45 @@
+import discord
+import asyncio
+import requests
+import json
+from discord.ext import commands
+if not discord.opus.is_loaded():
+    # the 'opus' library here is opus.dll on windows
+    # or libopus.so on linux in the current directory
+    # you should replace this with the location the
+    # opus library is located in and with the proper filename.
+    # note that on windows this DLL is automatically provided for you
+    discord.opus.load_opus('opus')
+client = discord.Client()
+user = 'iX7bgNY7pj4BBLsb'
+key = 'Rs1syy4IJqRnufruCtR07zu6ZwiIx0Pz'
+
+@client.event
+async def on_ready():
+    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | '+str(len(client.servers))+' servers')
+    await client.change_presence(game=discord.Game(name='SquarerFive'))
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!BotInfo'):
+        await client.send_message(message.channel, 'Bot programmed by SquarerFive, made in Python 3. I am an A.I and I will destroy all humans | More features coming soon.')
+    
+    elif not message.author.bot:
+     #   await client.send_typing(message.channel)
+        txt = message.content.replace(message.server.me.mention,'') if message.server else message.content
+        r = json.loads(requests.post('https://cleverbot.io/1.0/ask', json={'user':user, 'key':key, 'nick':'frost', 'text':txt}).text)
+        if r['status'] == 'success':
+            await client.send_message(message.channel, r['response'] )
+    
+print('Starting...')
+requests.post('https://cleverbot.io/1.0/create', json={'user':user, 'key':key, 'nick':'frost'})
+client.run('MzgwNjA3NDc0MTA3MTU0NDM1.DO7D3w.WA1SjqA0KjyFup0868ddHY6FldU')
+
+@client.event
+async def on_exit():
+    await client.send_message(message.channel, 'ALDI bot has gone offline :(')
+
+
+print("test")
+
+quit()
