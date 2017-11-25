0312aa831f523bf89996225cb5640d0306f9ab6f
SquarerFive <aidensoedjarwo@gmail.com>
2017-11-25 17:36:12 +1100

    Update SquarerSix.py

--- SquarerSix.py
+++ SquarerSix.py
@@ -3,13 +3,6 @@ import asyncio
 import requests
 import json
 from discord.ext import commands
-if not discord.opus.is_loaded():
-    # the 'opus' library here is opus.dll on windows
-    # or libopus.so on linux in the current directory
-    # you should replace this with the location the
-    # opus library is located in and with the proper filename.
-    # note that on windows this DLL is automatically provided for you
-    discord.opus.load_opus('opus')
 client = discord.Client()
 user = 'iX7bgNY7pj4BBLsb'
 key = 'Rs1syy4IJqRnufruCtR07zu6ZwiIx0Pz'
