58bf7705dd9de222a92e6074c3bd400cc22ba252
SquarerFive <aidensoedjarwo@gmail.com>
2017-12-20 16:30:15 +1100

    Update VerificationBot.py

--- VerificationBot.py
+++ VerificationBot.py
@@ -5,13 +5,7 @@ import json
 from discord.ext import commands
 from random import randint
 x = 0
-if not discord.opus.is_loaded():
-    # the 'opus' library here is opus.dll on windows
-    # or libopus.so on linux in the current directory
-    # you should replace this with the location the
-    # opus library is located in and with the proper filename.
-    # note that on windows this DLL is automatically provided for you
-    discord.opus.load_opus('opus')
+
 client = discord.Client()
 user = 'iX7bgNY7pj4BBLsb'
 key = 'Rs1syy4IJqRnufruCtR07zu6ZwiIx0Pz'
