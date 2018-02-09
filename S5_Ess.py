e6dc9b3c73a363d198677425c465361779aff6ab
SquarerFive <aidensoedjarwo@gmail.com>
2018-02-10 10:25:07 +1100

    Add files via upload

--- S5_Ess.py
+++ S5_Ess.py
@@ -71,19 +71,57 @@ async def info(ctx):
     b=ctx.message.channel
     c=ctx.message.author
     mainInfo= """[ALDI] Administrative Services, formerly known as SquarerFive_Essentials.
+**Version:** 1.0.1 // //
 **Hosted Locations:**
 *France [Redundency Server]*
 *Sydney, Australia [Blue Mountains]*
 *Hong Kong[Backup Server]*"""
-    otherInfo= """Creator: SquarerFive [id #4325].
-Contacts:
-**http://squarerfive.tk/**
-**http://www.youtube.com/SquarerFiveStudios/**
+    otherInfo= """
+Creator: SquarerFive [id #4325].
+**Contacts:**
+*http://squarerfive.tk/*
+*http://www.youtube.com/SquarerFiveStudios/*
+__**Hosted Platform:**__ __**Main Server**__
+**Linux CentOS 7**       **Hong Kong[Currently]**
+**Windows 10**           **Sydney, Autralia[Blue Mountains]**
           """
     em = discord.Embed(title='[ALDI] Administrative Services', description=mainInfo, colour=0x0055FF)
     em2 = discord.Embed(title='[ALDI] Administrative Services', description=otherInfo, colour=0x0055FF)
     await Client.send_message(b, embed=em)
     await Client.send_message(b, embed=em2)
+
+# rules
+@Client.command(pass_context = True)
+async def rules(ctx):
+    a=ctx.message.content
+    b=ctx.message.channel
+    c=ctx.message.author
+    rules="""
+**Rules of conduct:**
+**You** are responsible of what you type and say.
+Any abuse against members and staff will result in a warning.
+If a member reaches a threshold of **3 Warnings**, you will be **Banned** or **Kicked** depending on the violation.
+Bots do not kick members randomly.
+
+**/ Do and Don'ts /**
+**You Can:**
+Post Discord links in #advertisements.
+Post YouTube/Twitch links in #media.
+Curse passively.
+Talk freely.
+**/------/**
+**You Can't:**
+**You may not** post any images or content that is related to porn or gore/death.
+**You may not** create multiple accounts for spam or to avoid the bans (IPs are tracked).
+**You may not** post Discord Links in channels that are not #advertisements.
+
+**/-----/**
+These rules are subject to **change** at **any time**.
+Discord ToS/Guidelines apply here too.
+"""
+    em = discord.Embed(title='[ALDI] Administrative Services', description=rules, colour=0x0055FF)
+    await Client.send_message(b, embed=em)
+
     
 #@Client.event
 
